7c0c14a5ad575f804dadd3f8b3e091cfa5b54ff9
Remo Blaser <remo.blaser@jls.ch>
2020-03-04 15:34:08 +0100

    Fix import

--- android/src/main/java/com/oney/WebRTCModule/RTCVideoViewManager.java
+++ android/src/main/java/com/oney/WebRTCModule/RTCVideoViewManager.java
@@ -2,6 +2,7 @@ package com.oney.WebRTCModule;
 
 import android.support.annotation.Nullable;
 
+import com.facebook.react.common.MapBuilder;
 import com.facebook.react.uimanager.annotations.ReactProp;
 import com.facebook.react.uimanager.SimpleViewManager;
 import com.facebook.react.uimanager.ThemedReactContext;
@@ -20,27 +21,23 @@ public class RTCVideoViewManager extends SimpleViewManager<WebRTCView> {
   public WebRTCView createViewInstance(ThemedReactContext context) {
     return new WebRTCView(context);
   }
-  
+
   @Override
   public @Nullable Map getExportedCustomDirectEventTypeConstants() {
-    return MapBuilder.of(
-            "frameLayout",
-            MapBuilder.of("registrationName", "onFrameLayout")
-    );
+    return MapBuilder.of("frameLayout", MapBuilder.of("registrationName", "onFrameLayout"));
   }
 
-
   /**
-   * Sets the indicator which determines whether a specific {@link WebRTCView}
-   * is to mirror the video specified by {@code streamURL} during its rendering.
-   * For more details, refer to the documentation of the {@code mirror} property
-   * of the JavaScript counterpart of {@code WebRTCView} i.e. {@code RTCView}.
+   * Sets the indicator which determines whether a specific {@link WebRTCView} is
+   * to mirror the video specified by {@code streamURL} during its rendering. For
+   * more details, refer to the documentation of the {@code mirror} property of
+   * the JavaScript counterpart of {@code WebRTCView} i.e. {@code RTCView}.
    *
-   * @param view The {@code WebRTCView} on which the specified {@code mirror} is
-   * to be set.
+   * @param view   The {@code WebRTCView} on which the specified {@code mirror} is
+   *               to be set.
    * @param mirror If the specified {@code WebRTCView} is to mirror the video
-   * specified by its associated {@code streamURL} during its rendering,
-   * {@code true}; otherwise, {@code false}.
+   *               specified by its associated {@code streamURL} during its
+   *               rendering, {@code true}; otherwise, {@code false}.
    */
   @ReactProp(name = "mirror")
   public void setMirror(WebRTCView view, boolean mirror) {
@@ -49,15 +46,15 @@ public class RTCVideoViewManager extends SimpleViewManager<WebRTCView> {
 
   /**
    * In the fashion of
-   * https://www.w3.org/TR/html5/embedded-content-0.html#dom-video-videowidth
-   * and https://www.w3.org/TR/html5/rendering.html#video-object-fit, resembles
-   * the CSS style {@code object-fit}.
+   * https://www.w3.org/TR/html5/embedded-content-0.html#dom-video-videowidth and
+   * https://www.w3.org/TR/html5/rendering.html#video-object-fit, resembles the
+   * CSS style {@code object-fit}.
    *
-   * @param view The {@code WebRTCView} on which the specified {@code objectFit}
-   * is to be set.
+   * @param view      The {@code WebRTCView} on which the specified
+   *                  {@code objectFit} is to be set.
    * @param objectFit For details, refer to the documentation of the
-   * {@code objectFit} property of the JavaScript counterpart of
-   * {@code WebRTCView} i.e. {@code RTCView}.
+   *                  {@code objectFit} property of the JavaScript counterpart of
+   *                  {@code WebRTCView} i.e. {@code RTCView}.
    */
   @ReactProp(name = "objectFit")
   public void setObjectFit(WebRTCView view, String objectFit) {
@@ -71,12 +68,12 @@ public class RTCVideoViewManager extends SimpleViewManager<WebRTCView> {
 
   /**
    * Sets the z-order of a specific {@link WebRTCView} in the stacking space of
-   * all {@code WebRTCView}s. For more details, refer to the documentation of
-   * the {@code zOrder} property of the JavaScript counterpart of
-   * {@code WebRTCView} i.e. {@code RTCView}.
+   * all {@code WebRTCView}s. For more details, refer to the documentation of the
+   * {@code zOrder} property of the JavaScript counterpart of {@code WebRTCView}
+   * i.e. {@code RTCView}.
    *
-   * @param view The {@code WebRTCView} on which the specified {@code zOrder} is
-   * to be set.
+   * @param view   The {@code WebRTCView} on which the specified {@code zOrder} is
+   *               to be set.
    * @param zOrder The z-order to set on the specified {@code WebRTCView}.
    */
   @ReactProp(name = "zOrder")
